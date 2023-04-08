82d90ef118d04fce3fe88e27f0b628b33de3e0b3
Kendall Williams <kendallowilliams@outlook.com>
2023-04-08 12:16:23 -0400

    removed unneeded files

--- media-library/apps/ml-app/src/app/app-settings/app-settings.component.ts
+++ media-library/apps/ml-app/src/app/app-settings/app-settings.component.ts
@@ -1,38 +1,10 @@
-import { Component, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { SelectValueComparer } from '@media-library/ml-ui';
-import { SelectOption } from '@media-library/ml-ui';
-import { Subject } from 'rxjs';
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './app-settings.component.html',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppSettingsComponent {
-  protected selectedOption = 'A';
-  protected form: FormGroup;
-  protected indices$ = new Subject<number[]>();
-  protected valueComparerFn: SelectValueComparer<string> = (obj1: string, obj2: string) => obj1 === obj2;
-
-  constructor(private _fb: FormBuilder) {
-    this.form = this._fb.group({
-      option: new FormControl(['A'])
-    });
-    
-    const arr: number[] = [];
-
-    window.setInterval(() => {
-      if (arr.length < 10) arr.push(arr.length);
-      this.indices$.next(arr);
-    }, 2000);
-  }
-
-  public submit() : void {
-    console.log(this.form.controls['option'].value);
-  }
-
-  public handleChange(evt: SelectOption<string> | null) : void {
-    console.log(evt?.value);
-  }
 }
