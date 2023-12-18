5f066b91c6ac23e9890403df1f7bb1526b2c0241
Kendall Williams <kendallowilliams@outlook.com>
2023-12-17 21:37:44 -0500

    build fix

--- media-library/libs/ml-data/src/index.ts
+++ media-library/libs/ml-data/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib//stores/configurations/configurations.models';
 export * from './lib//stores/configurations/configurations.selectors';
 export * from './lib//stores/configurations/configurations.reducer';
 export * from './lib//stores/configurations/configurations.actions';
