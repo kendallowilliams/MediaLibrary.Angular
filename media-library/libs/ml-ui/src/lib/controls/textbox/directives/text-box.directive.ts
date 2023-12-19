c26bf751c9fe74efbc9f4f25d2c8756ab58a13ad
Kendall Williams <kendallowilliams@outlook.com>
2023-12-19 15:43:56 -0500

    textbox style

--- media-library/libs/ml-ui/src/lib/controls/textbox/directives/text-box.directive.ts
+++ media-library/libs/ml-ui/src/lib/controls/textbox/directives/text-box.directive.ts
@@ -4,7 +4,7 @@ import { Directive, HostBinding } from "@angular/core";
   selector: '[mlTextBox]'
 })
 export class TextBoxDirective {
-  @HostBinding('class') private _class = `h-control text-dark dark:text-light border-dark dark:border-light 
-    border-[1px] border-solid rounded-[5px] outline-none p-[5px] bg-light dark:bg-dark focus:ring inline-block w-full`;
+  @HostBinding('class') private _class = `h-control text-dark border-dark
+    border-[1px] border-solid rounded-[5px] outline-none p-[5px] dark:bg-light focus:ring inline-block w-full`;
   @HostBinding('attr.type') private _type = 'text';
 }
