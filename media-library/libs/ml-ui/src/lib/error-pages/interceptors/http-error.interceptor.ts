467e02163d7524847c4aae498dfb0657cad2860e
Kendall Williams <kendallowilliams@outlook.com>
2023-12-20 19:56:49 -0500

    minor change

--- media-library/libs/ml-ui/src/lib/error-pages/interceptors/http-error.interceptor.ts
+++ media-library/libs/ml-ui/src/lib/error-pages/interceptors/http-error.interceptor.ts
@@ -6,7 +6,8 @@ import { Observable, catchError, throwError } from "rxjs";
 export class HttpErrorIntercepter implements HttpInterceptor {
   public intercept(req: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     return next.handle(req)
-      .pipe(catchError((response: HttpErrorResponse) => {let errorMsg = '';
+      .pipe(catchError((response: HttpErrorResponse) => {
+        let errorMsg = '';
         if (response.error instanceof ErrorEvent) {
           /** Scripts/File Errors */
           errorMsg = `Error: ${response.error.message}`;
