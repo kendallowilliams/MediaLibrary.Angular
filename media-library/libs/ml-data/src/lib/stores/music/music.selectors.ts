e143c1f43eada886457eecf62d2ddf336580a171
Kendall Williams <kendallowilliams@outlook.com>
2023-12-20 23:10:34 -0500

    minor change

--- media-library/libs/ml-data/src/lib/stores/music/music.selectors.ts
+++ media-library/libs/ml-data/src/lib/stores/music/music.selectors.ts
@@ -1,4 +1,4 @@
-import { createFeatureSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 import {
   MUSIC_FEATURE_KEY, MusicState,
 } from './music.reducer';
@@ -6,4 +6,19 @@ import {
 // Lookup the 'Music' feature state managed by NgRx
 export const selectMusicState =
   createFeatureSelector<MusicState>(MUSIC_FEATURE_KEY);
+
+export const selectAllAlbums = createSelector(
+  selectMusicState,
+  (state: MusicState) => state.albums
+);
+
+export const selectAllArtists = createSelector(
+  selectMusicState,
+  (state: MusicState) => state.artists
+);
+
+export const selectAllTracks = createSelector(
+  selectMusicState,
+  (state: MusicState) => state.tracks
+);
 
