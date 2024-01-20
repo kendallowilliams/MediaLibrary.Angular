b888c41834af776dfffad5bd970ca9876598d397
Kendall Williams <kendallowilliams@outlook.com>
2024-01-20 10:17:40 -0500

    message box changes

--- media-library/libs/ml-ui/src/lib/message-box/storybook/message-box.component.stories.ts
+++ media-library/libs/ml-ui/src/lib/message-box/storybook/message-box.component.stories.ts
@@ -38,7 +38,7 @@ export const Default: Story = {
     template: `
       <div class="flex justify-center">
         <button mlButton [variant]="'primary'" (click)="isOpen=true">Show&nbsp;<span class="capitalize">{{messageType}}</span></button>
-        <ng-template mlDismissableModal [(isOpen)]="isOpen" [useAppRootVcr]="false">
+        <ng-template mlModal [(isOpen)]="isOpen" [useAppRootVcr]="false">
           <ml-message-box [title]="title" [message]="message" [messageType]="messageType"></ml-message-box>
         </ng-template>
       </div>
