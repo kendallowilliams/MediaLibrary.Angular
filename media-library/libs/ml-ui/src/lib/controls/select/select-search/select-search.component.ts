0e666f438867986277253b0300f7823799be6875
Kendall Williams <kendallowilliams@outlook.com>
2024-01-21 22:33:58 -0500

    select search changes

--- media-library/libs/ml-ui/src/lib/controls/select/select-search/select-search.component.ts
+++ media-library/libs/ml-ui/src/lib/controls/select/select-search/select-search.component.ts
@@ -1,10 +1,13 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  ElementRef,
   EventEmitter,
   HostBinding,
+  HostListener,
   Input,
   Output,
+  Renderer2,
   ViewEncapsulation,
 } from '@angular/core';
 import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
@@ -15,18 +18,36 @@ import { faSearch, faTimes } from '@fortawesome/free-solid-svg-icons';
   changeDetection: ChangeDetectionStrategy.Default,
 })
 export class SelectSearchComponent {
-  @HostBinding('class') private _class = `flex h-control text-dark border-dark px-[5px] m-[5px]
-    border-[1px] border-solid rounded-[5px] outline-none dark:bg-light focus:ring inline-block`;
-
   @Input() public placeholder = '';
 
   public query = '';
   @Output() public queryChange = new EventEmitter<string>();
 
+  @HostBinding('class') private _class = `flex h-control text-dark border-dark px-[5px] m-[5px]
+    border-[1px] border-solid rounded-[5px] outline-none bg-white
+    items-center`;
+  @HostBinding('attr.tabindex') private _tabIndex = 0;
+
   public faTimes = faTimes;
   public faSearch = faSearch;
 
+  constructor(private _host: ElementRef<HTMLElement>, private _renderer: Renderer2) {}
+
   public handleQueryChange(query: string): void {
     this.queryChange.emit(query);
+  }
+
+  @HostListener('focusin')
+  private _handleFocus() : void {
+    this._renderer.addClass(this._host.nativeElement, 'ring');
+  }
+
+  @HostListener('focusout')
+  private _handleBlur() : void {
+    this._renderer.removeClass(this._host.nativeElement, 'ring');
+  }
+
+  public handleClear() : void {
+    this.query = '';
   }
 }
--- media-library/libs/ml-ui/src/lib/controls/text-box/directives/text-box.directive.ts
+++ media-library/libs/ml-ui/src/lib/controls/text-box/directives/text-box.directive.ts
@@ -6,6 +6,6 @@ import { Directive, HostBinding } from "@angular/core";
 })
 export class TextBoxDirective {
   @HostBinding('class') private _class = `h-control text-dark border-dark
-    border-[1px] border-solid rounded-[5px] outline-none p-[5px] dark:bg-light focus:ring inline-block w-full`;
+    border-[1px] border-solid rounded-[5px] outline-none p-[5px] bg-white focus:ring inline-block w-full`;
   @HostBinding('attr.type') private _type = 'text';
 }
