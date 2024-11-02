e964a15c54306bfed586653e0e91d4b96d5bc256
Kendall Williams <kendallowilliams@outlook.com>
2024-11-01 23:15:45 -0400

    minor changes

--- media-library/libs/ml-ui/.storybook/main.ts
+++ media-library/libs/ml-ui/.storybook/main.ts
@@ -38,6 +38,7 @@ const config: StorybookConfig = {
     name: '@storybook/angular',
     options: {},
   },
+  staticDirs: [ {from: '../src/assets/images', to: '/static' }]
 };
 
 export default config;
